c6614189ffd43c7ee31db93a9e2c5be94c2f9f27
Pavel Malave <pavel.malave-external@globalpay.com>
2024-03-09 12:21:34 +0100

    changes

--- src/main/java/com/example/demo/precios/DemoPreciosApplication.java
+++ src/main/java/com/example/demo/precios/DemoPreciosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoPreciosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoPreciosApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoPreciosApplication.class, args);
+  }
 
 }
