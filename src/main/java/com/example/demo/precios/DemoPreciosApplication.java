6cff2a82f8dcc534a06e12a1c9878fafb5313274
Pavel Malave <pavel.malave-external@globalpay.com>
2024-03-09 11:57:18 +0100

    spring boot base

---
+++ src/main/java/com/example/demo/precios/DemoPreciosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.precios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPreciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPreciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/precios/DemoPreciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.precios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPreciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
